a30a6d45db062cd9c1a567838271bf66e2a12260
Zakari Adamu <zakari.adamu714@gmail.com>
2026-04-05 22:01:08 +0100

    Delete expense-tracker-api directory

--- expense-tracker-api/src/routes/dashboardRoute.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getDashboardData } from "../controllers/dashboardController";
-
-const dashboardRouter = express.Router();
-
-dashboardRouter.get("/", getDashboardData);
-export default dashboardRouter;
