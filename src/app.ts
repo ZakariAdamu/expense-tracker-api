2d57a3c8ec4e994c6aa3881c4d96aadea75eaf63
Zakari <zakari.adamu714@gmail.com>
2026-05-12 10:39:14 +0100

    refactor: update rate limit to 15 trials in 15 minutes

--- src/app.ts
+++ src/app.ts
@@ -60,7 +60,7 @@ export function createApp() {
 
   const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 20, // limit each IP
+    max: 15, // limit each IP
     standardHeaders: true,
     legacyHeaders: false,
   });
